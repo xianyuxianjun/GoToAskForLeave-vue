facada3377ee1ab16b6c9b0b2da609d79e64c304
陈咸鱼 <1796792003@qq.com>
2024-11-09 17:29:42 +0800

    项目初始化

---
+++ src/pages/login.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { useGenerateImageVariant } from '@/@core/composable/useGenerateImageVariant'
+import authV1LoginMaskDark from '@images/pages/auth-v1-login-mask-dark.png'
+import authV1LoginMaskLight from '@images/pages/auth-v1-login-mask-light.png'
+import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
+import { themeConfig } from '@themeConfig'
+
+definePage({ meta: { layout: 'blank' } })
+
+const form = ref({
+  email: '',
+  password: '',
+  remember: false,
+})
+
+const login =  () => {
+  const res =  useApi('/login')
+  console.log(res)
+}
+
+const authV1ThemeLoginMask = useGenerateImageVariant(authV1LoginMaskLight, authV1LoginMaskDark)
+const isPasswordVisible = ref(false)
+</script>
+
+<template>
+  <div class="auth-wrapper d-flex align-center justify-center pa-4">
+    <VCard
+      class="auth-card pa-1 pa-sm-7"
+      max-width="448"
+    >
+      <VCardItem class="justify-center pb-6">
+        <VCardTitle>
+            <div class="app-logo">
+              <VNodeRenderer :nodes="themeConfig.app.logo" />
+              <h1 class="app-logo-title">
+                {{ themeConfig.app.title }}
+              </h1>
+            </div>
+        </VCardTitle>
+      </VCardItem>
+      <VCardText>
+        <VForm>
+          <VRow>
+            <!-- email -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.email"
+                autofocus
+                label="Email"
+                type="email"
+                placeholder="johndoe@email.com"
+              />
+            </VCol>
+
+            <!-- password -->
+            <VCol cols="12">
+              <VTextField
+                v-model="form.password"
+                label="Password"
+                placeholder="············"
+                :type="isPasswordVisible ? 'text' : 'password'"
+                :append-inner-icon="isPasswordVisible ? 'ri-eye-off-line' : 'ri-eye-line'"
+                @click:append-inner="isPasswordVisible = !isPasswordVisible"
+              />
+
+              <!-- remember me checkbox -->
+              <div class="d-flex align-center justify-space-between flex-wrap my-6">
+                <VCheckbox
+                  v-model="form.remember"
+                  label="Remember me"
+                />
+              </div>
+
+              <!-- login button -->
+              <VBtn
+                block
+                @click="login"
+              >
+                Login
+              </VBtn>
+            </VCol>
+
+            <!-- create account -->
+            <VCol
+              cols="12"
+              class="text-body-1 text-center"
+            >
+            </VCol>
+          </VRow>
+        </VForm>
+      </VCardText>
+    </VCard>
+    <VImg
+      :src="authV1ThemeLoginMask"
+      class="d-none d-md-block auth-footer-mask flip-in-rtl"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@core/scss/template/pages/page-auth.scss";
+</style>

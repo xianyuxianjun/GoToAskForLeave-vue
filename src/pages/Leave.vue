a3a471f7c535af60453b2fb13aaed4ed82246674
陈咸鱼 <1796792003@qq.com>
2024-11-26 00:21:16 +0800

    导出请假名单

--- src/pages/Leave.vue
+++ src/pages/Leave.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useUserStore } from "@/store/user.js"
+import * as XLSX from 'xlsx';
 const userStore = useUserStore();
 import LeaveDialog from "@/views/LeaveDialog.vue";
 import {onMounted, ref} from "vue";
@@ -57,6 +58,42 @@ function ddale(mes){
   message.value = mes
   dale.value = true
 }
+const exportToExcel = () => {
+  // 自定义表头
+  const headers = [
+    { header: '审核时间', key: 'audittime' },
+    { header: '班级名称', key: 'className' },
+    { header: '课程名称', key: 'courseName' },
+    { header: '请假天数', key: 'daynum' },
+    { header: '请假ID', key: 'leaveId' },
+    { header: '意见', key: 'opinion' },
+    { header: '理由', key: 'reason' },
+    { header: '状态', key: 'status' },
+    { header: '学生ID', key: 'stuId' },
+    { header: '学生姓名', key: 'stuName' }
+  ];
+
+  // 创建一个空的工作表
+  const worksheet = XLSX.utils.json_to_sheet([]);
+
+  // 添加自定义表头
+  XLSX.utils.sheet_add_aoa(worksheet, [headers.map(h => h.header)]);
+
+  // 将数据添加到工作表中
+  leaveList.value.forEach(row => {
+    const rowData = headers.map(header => row[header.key]);
+    XLSX.utils.sheet_add_aoa(worksheet, [rowData], { origin: -1 });
+  });
+
+  // 创建工作簿
+  const workbook = XLSX.utils.book_new();
+
+  // 将工作表添加到工作簿中
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+  // 生成并下载 Excel 文件
+  XLSX.writeFile(workbook, 'leave.xlsx');
+};
 </script>
 
 <template>
@@ -74,6 +111,9 @@ function ddale(mes){
         </VCol>
         <VCol cols="12" md="2">
           <VSelect label="班级" :items="classList" v-model="classes" @change="select"/>
+        </VCol>
+        <VCol cols="12" md="2">
+          <VBtn @click="exportToExcel">导出请假名单</VBtn>
         </VCol>
       </VRow>
     </VCardText>
